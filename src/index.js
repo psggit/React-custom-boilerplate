51a6d04355d76dc0686f1b42164dc595ea2af2b2
Aparna Chinnaiah <achinna@achinna-in-la01.eu.trimblecorp.net>
2020-01-22 11:19:15 +0530

    added lint files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "Sass/app.scss"
+
+export function App() {
+  return (
+    <div>Basic App Setup</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
